c9cd5b7cfbaff0882dbc1965867e5556c2496476
Oskar-Kokk <61141978+Oskar-Kokk@users.noreply.github.com>
2020-02-25 10:27:55 +0100

    Player2 mover script
    This script is like the Mover.cs script, but for player2

---
+++ Mover2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover2 : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
