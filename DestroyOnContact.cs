9f4825ffc17acaf35f926dece2cd9f5169dc2267
Oskar-Kokk <61141978+Oskar-Kokk@users.noreply.github.com>
2020-02-25 10:35:16 +0100

    Destroy on contact
    This script tells the ship how to behave when it comes into contact with other objects, for example to destroy when it gets shot.

---
+++ DestroyOnContact.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Oi mate, I can't find the bloody 'GameController' script mate. Fookin' shite, innit?");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        { 
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
